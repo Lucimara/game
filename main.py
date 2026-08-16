fbd34e1f329f23497680eb990299dd0718a7ef76
LucimaraBL <marinhaad@gmail.com>
2026-08-16 19:27:59 -0300

    Comite inicial.

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+print('Início do Jogo!')
+pygame.init()
+
+window = pygame.display.set_mode(size=(600, 600))
+
+while True:
+    # Checando todos os eventos
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()  # Fecha janela
+            print('Fim do Jogo!')
+            quit()  # Finaliza o pygame
